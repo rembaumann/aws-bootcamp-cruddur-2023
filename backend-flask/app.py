d39d37e17b213ab26274786fda2055e33ff97389
Rem Baumann <baumann.rem@gmail.com>
2023-02-28 22:55:02 +0000

    implement cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,12 +15,25 @@ from services.create_message import *
 from services.show_activity import *
 
 # X-Ray
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
+# Cloudwatch logs
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("test log")
 
 # Honeycomb
 from opentelemetry import trace
@@ -46,7 +59,7 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 # X-Ray
-XRayMiddleware(app, xray_recorder)
+# XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
@@ -62,6 +75,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Cloudwatch logs
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -100,7 +120,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger= LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    segment = xray_recorder.begin_segment('user_activities')
+    # segment = xray_recorder.begin_segment('user_activities')
 
     model = {
       'errors': None,
@@ -25,13 +25,13 @@ class UserActivities:
       }]
       model['data'] = results
 
-    subsegment = xray_recorder.begin_subsegment('mock-data')
+    # # X-Ray
+    # subsegment = xray_recorder.begin_subsegment('mock-data')
 
-      # X-Ray
-    dict = {
-      "now": now.isoformat(),
-      "results-size": len(model['data'])
-    }
-    segment.put_metadata('key', dict, 'namespace')
+    # dict = {
+    #   "now": now.isoformat(),
+    #   "results-size": len(model['data'])
+    # }
+    # segment.put_metadata('key', dict, 'namespace')
     
     return model
