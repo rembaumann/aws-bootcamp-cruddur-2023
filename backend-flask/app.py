2a579b5522c1a5e7be7a72fb8e0c08a684e10b65
Rem Baumann <baumann.rem@gmail.com>
2023-03-01 00:10:21 +0000

    midway through Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -55,8 +55,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # Show this in the logs within the backend flask app(STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
@@ -77,7 +77,8 @@ def init_rollbar():
     """init rollbar module"""
     rollbar.init(
         # access token
-        rollbar_access_token,
+        # rollbar_access_token,
+        '9ef4247e1f15440e90be1f2055bb6894',
         # environment name
         'production',
         # server root directory, makes tracebacks prettier
