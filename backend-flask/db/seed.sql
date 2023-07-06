c8c53686645aed1a8a37671356d295a0e0c4c94a
Rem Baumann <baumann.rem@gmail.com>
2023-07-06 15:58:07 +0000

    add cognito user id to db seed data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Andrew Brown','baumann.rem+andrewbrown@gmail.com' , 'andrewbrown' ,'c6e42a56-448e-4f01-a411-58dc5518f14e'),
+  ('Andrew Bayko','baumann.rem+bayko@gmail.com' , 'bayko' ,'32ed0d50-ff0e-47bb-b801-319a62c2818e'),
   ('Rem Baumann', 'baumann.rem@gmail.com','rembaumann','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
