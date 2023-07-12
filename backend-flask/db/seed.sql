d3f1e739ec891e00f5c71d54a096a265945f4db3
Rem Baumann <baumann.rem@gmail.com>
2023-07-12 17:19:21 +0000

    revert seed data to 'MOCK' for cognito id

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','baumann.rem+andrewbrown@gmail.com' , 'andrewbrown' ,'c6e42a56-448e-4f01-a411-58dc5518f14e'),
-  ('Andrew Bayko','baumann.rem+bayko@gmail.com' , 'bayko' ,'32ed0d50-ff0e-47bb-b801-319a62c2818e'),
+  ('Andrew Brown','baumann.rem+andrewbrown@gmail.com' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','baumann.rem+bayko@gmail.com' , 'bayko' ,'MOCK'),
   ('Rem Baumann', 'baumann.rem@gmail.com','rembaumann','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
